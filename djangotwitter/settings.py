1a6b0fb4ba2f8df11886d56a2baf7e36f2c36b70
myerkes <mattyerkes23@gmail.com>
2021-06-30 17:12:50 -0400

    Update requirements.txt for correct venv

--- djangotwitter/settings.py
+++ djangotwitter/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'djangotwitter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bpdugcgp',
+        'USER': 'bpdugcgp',
+        'PASSWORD': 'Mmjfs7LHZWSlBZ3s5oOmtTIpzICLMl4Z',
+        'HOST': 'batyr.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
 
