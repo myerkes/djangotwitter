cbda5b9e85b6d29fcb218d1364bd8df062a19e11
myerkes <mattyerkes23@gmail.com>
2021-07-01 13:14:01 -0400

    Add login_required and loginrequiredmixin to views.py

--- posts/views.py
+++ posts/views.py
@@ -3,12 +3,15 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 # Create your views here.
 def index(request):
     return render(request, 'posts/index.html')
 
 ### Post Views ###
-class PostList(ListView):
+class PostList(LoginRequiredMixin, ListView):
     model = Post
     context_object_name = 'post_list'
     template_name = 'posts/post_list.html'
