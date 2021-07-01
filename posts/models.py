da6d1763c0c7c7d9b347e6849160b193b0c63e32
myerkes <mattyerkes23@gmail.com>
2021-06-30 22:03:19 -0400

    Create Post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    pub_datetime = models.DateTimeField(auto_now=False, auto_now_add=True)
+    post_text = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.post_text
+
+
