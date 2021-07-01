89ae66abe06a3a2517f6c24f00e71585589716a9
myerkes <mattyerkes23@gmail.com>
2021-07-01 10:15:12 -0400

    Create ReplyList view

--- posts/urls.py
+++ posts/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'posts'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('posts/', views.PostList.as_view(), name='posts')
+    path('posts/', views.PostList.as_view(), name='posts'),
+    path('<int:pk>/replies', views.ReplyList.as_view(), name='replies'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,19 @@
-from posts.models import Post
+from posts.models import Post, Reply
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 # Create your views here.
 def index(request):
     return render(request, 'posts/index.html')
 
+### Post Views ###
 class PostList(ListView):
     model = Post
     context_object_name = 'post_list'
     template_name = 'posts/post_list.html'
+
+### Reply Views ###
+class ReplyList(DetailView):
+    model = Post
+    template_name = 'posts/reply_list.html'
