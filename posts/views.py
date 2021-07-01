b484d56b14ef9df1d08794d5b3c931675743831f
myerkes <mattyerkes23@gmail.com>
2021-07-01 09:48:03 -0400

    Create PostList view

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,13 @@
+from posts.models import Post
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 # Create your views here.
 def index(request):
     return render(request, 'posts/index.html')
+
+class PostList(ListView):
+    model = Post
+    context_object_name = 'post_list'
+    template_name = 'posts/post_list.html'
