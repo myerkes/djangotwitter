cdc509ca52b0c93b08c661a6e7f8e579d6e89305
myerkes <mattyerkes23@gmail.com>
2021-06-30 22:06:05 -0400

    Create Reply model

--- posts/models.py
+++ posts/models.py
@@ -2,10 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    pub_datetime = models.DateTimeField(auto_now=False, auto_now_add=True)
     post_text = models.CharField(max_length=255)
+    pub_datetime = models.DateTimeField(auto_now=False, auto_now_add=True)
 
     def __str__(self) -> str:
         return self.post_text
 
 
+class Reply(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    reply_text = models.CharField(max_length=255)
+    pub_datetime = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.reply_text
