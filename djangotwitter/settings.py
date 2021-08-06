caf8a46628eba29a02d83e071f184eb0384a7cae
Matthew Yerkes <mattyerkes23@gmail.com>
2021-08-06 15:46:57 -0400

    Debug off

--- djangotwitter/settings.py
+++ djangotwitter/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-za&$d%t0cmuztgn#)2(!n&+mx1a1o$r_w1s*d$%2omm4d-*q6@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
