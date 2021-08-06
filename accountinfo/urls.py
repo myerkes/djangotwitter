7650348f50c9cfe10e287c1e20f32fd02ffda849
Matthew Yerkes <mattyerkes23@gmail.com>
2021-08-06 14:43:17 -0400

    Create user profile by user id

--- accountinfo/urls.py
+++ accountinfo/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'myaccount'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('profile/', views.profile, name='profile'),
+    path('/profile/', views.profile, name='profile'),
+    path('<int:user_pk>/profile/', views.Profile2.as_view(), name='profile2'),
 ]
--- accountinfo/views.py
+++ accountinfo/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from posts.models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
@@ -6,3 +10,13 @@ def index(request):
 
 def profile(request):
     return render(request, 'accountinfo/profile.html')
+
+class Profile2(LoginRequiredMixin, ListView):
+    model = Post
+    template_name = 'posts/post_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        posts = Post.objects.filter(poster=self.kwargs['user_pk'])
+        context['post_list'] = posts
+        return context
